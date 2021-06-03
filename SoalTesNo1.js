61f930db956d689abaae68d016d94a1b929f2edb
Ahmad Fazri Nursamsu <85002577+AhmadFazriNursamsi@users.noreply.github.com>
2021-06-03 10:44:41 +0700

    Update and rename SoalTesNo1.js to SoalTesNo1.html

--- SoalTesNo1.js
+++
@@ -1,4 +0,0 @@
-const is_password_valid = (password) =>  /(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[\W|_])(?=.*[=]).{8,}$/.test(password);
-
-console.log(is_password_valid('passW0rd='));
-console.log(is_password_valid('C0d3YourFuture!#'));
